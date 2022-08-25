5dbd5619aeaf8823911ee2c2dd7de0f5d9cef3d5
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-08-25 14:57:11 +0300

    верстаем страницу диÑалогов

--- src/components/Dilogs/Dilogs.tsx
+++ src/components/Dilogs/Dilogs.tsx
@@ -4,8 +4,31 @@ import D from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={D.dialogs}>
+            <div className={D.dialogsItems}>
+                <div className={D.dialog + ' ' + D.active}>
+                    Alex
+                </div>
+                <div className={D.dialog}>
+                    Alex1
+                </div>
+                <div className={D.dialog}>
+                    Alex2
+                </div>
+                <div className={D.dialog}>
+                    Alex3
+                </div>
+                <div className={D.dialog}>
+                    Alex4
+                </div>
+            </div>
+            <div className={D.message}>
+                <div className={D.messages}>
+                    <div className={D.message}>Hi</div>
+                    <div className={D.message}>How is ypur it&</div>
+                    <div className={D.message}>Yo</div>
+                </div>
+            </div>
         </div>
     )
 }
